19073bf3ae06b3af4b51a07e6ef2e72b842e4344
ilkay murat ertekin <iertekinx@gmail.com>
2021-05-11 16:42:58 +0300

    add commit

--- Trendyol/TrendyolData.py
+++ Trendyol/TrendyolData.py
@@ -30,19 +30,23 @@ class T_Product:
 
     def ProductDetail(self,product):
         #Trendyol Linki girilirse boşlukla değiştiriyor
+        # If Trendyol Link is entered, it replaces it with a space.
         product=product.replace('https://www.trendyol.com','')
         self.product_link=self.link+product
         print('Ürün Linki',self.product_link)
         
         #requests ve soup işlemleri
+        #requests and soup operations
         r=requests.get(self.product_link)
         soup=BeautifulSoup(r.content,'html.parser')
         
         # Ürün İsmi
+        #Product Name
         self.ProductName=soup.find('div',class_='pr-in-cn').span.text
         print(self.ProductName)
 
         #Kargo Ücretsiz mi sorgusu
+        # Free Shipping query
         if soup.find('div',class_='stamps').text:
             self.kargo=True
             print(self.kargo,soup.find('div',class_='stamps').text)
@@ -51,6 +55,7 @@ class T_Product:
             print(self.kargo,'KARGO BEDAVA DEĞİL')
 
         # Ürün Yıldız Sayısı
+        # Product Ratings
         if soup.find('div',class_='pr-in-rnr'):
             self.ProductRatings=soup.find('div',class_='ttl').span.text     
             print(self.ProductRatings)
@@ -59,6 +64,7 @@ class T_Product:
             print('Henüz Yorum Yok')
 
         # Ürünün ilk fiyatı indirimli fiyatı     
+        # Product original price and discounted price  
         self.ProductOrginalPrice=soup.find('div',class_='pr-bx-nm').find_all('span')
         for i in self.ProductOrginalPrice:
             if i['class'][0]=='prc-org':
@@ -70,6 +76,8 @@ class T_Product:
                 print('ikinci fiyat',self.second_prc)
         
         # Sepetteki İndirim oranı ve sepetteki indirimli fiyat
+        # if there is discount in cart 
+        # discount ratio and discounted price
         if soup.find('div',class_='pr-bx-dsc'):
             self.DscRatio=soup.find('div',class_='pr-bx-pr-dsc').div.text
             print(self.DscRatio)
@@ -79,6 +87,7 @@ class T_Product:
                 print('DSC PRİCE',i.text)
        
         # ürün beden bilgisi
+        # Product size
         if soup.find('div',class_='pr-in-at-sp'):
             self.inSize=[]
             self.outSize=[]
@@ -94,12 +103,14 @@ class T_Product:
             print('olmayan Bedenler:',self.outSize)
             print('Olan Bedenler :',self.inSize)
 
-        # Ürün Marka bilgisi 
+        # Ürün Marka bilgisi
+        # Product Brand 
         if soup.find('h1',class_='pr-new-br'):
             self.marka=soup.find('h1',class_='pr-new-br').text
             span=soup.find('h1',class_='pr-new-br').span.text
             self.marka=self.marka.replace(span,'')    
 
+        #Product image urls
         if soup.find('img',class_='ph-gl-img'):
             self.img_url=soup.find('img',class_='ph-gl-img')['src']
             print('img_url',self.img_url)
