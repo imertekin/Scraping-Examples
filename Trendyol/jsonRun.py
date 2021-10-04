d57f84c8479cbd2b481d37e18d5bdf8a7630ebcc
ilkay murat ertekin <iertekinx@gmail.com>
2021-10-05 00:55:02 +0300

    exception added

--- Trendyol/jsonRun.py
+++ Trendyol/jsonRun.py
@@ -1,6 +1,9 @@
 import psycopg2
 from psycopg2 import sql
+from requests.api import request
 from sqlalchemy import create_engine
+
+import requests
 
 from Trendyol import Json_class as js 
 
@@ -42,16 +45,21 @@ data=js.data_parser()
 def add():
     global df
     for i in words:
-        data.run(i,3)
-        df=data.data
+        try:
+            data.run(i,3)
+            df=data.data
+        except requests.exceptions.ConnectionError:
+            time.sleep(5)
+            continue
+        try:
+            df.drop_duplicates(subset=['P_id'],inplace=True)
         
-    try:
-        df.drop_duplicates(subset=['P_id'],inplace=True)
-        
-    except:
-        print('Error ',i)
+        except:
+            print('Error ',i)
 
 
 add() 
 
 df.to_sql('JsonProduct',engine,if_exists='append',index=False)
+
+
